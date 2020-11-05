97ffda0e5a109891970c18c3d88ae9d278d2ecc3
Fazi <socialsolution15@gmail.com>
2020-11-04 16:18:28 -0800

    updated new

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader />
+    <div class="main">
+    <router-view />
+    <AppFooter/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from "@/components/header/AppHeader";
+import AppFooter from "@/components/footer/AppFooter";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+    AppFooter
+  },
+};
 </script>
 
 <style>
+/* common css */
+.main {
+  position: relative;
+  top: 120px;
+}
+.container {
+  max-width: 1266px;
+  margin: 0 auto;
+}
+.btn {
+  border-radius: 50px;
+  text-decoration: none;
+  padding: 12px 42px;
+  font-weight: bold;
+  font-size: 18px;
+  text-transform: uppercase;
+  overflow: hidden;
+  display: block;
+  background-image: url("./assets/images/right-shadow.svg"),
+    url("./assets/images/right-chevron.svg");
+  background-position: 107% 100%, 90% 50%;
+  background-size: auto 140%, 6px 740px;
+  background-repeat: no-repeat, no-repeat;
+  color:#fff;
+  background-color: #3182CE;
+}
+/* common css */
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  font-family: Montserrat, sans-serif;
+  color: #282c3f;
+}
+body{
+  margin:0;
 }
 </style>
---
+++ src/components/live-project/Live.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <div class="project-heading">
+            <h2><span>Live</span> Projects</h2>
+        </div>
+        <div v-for="(card, index) in cards" :key="index">
+                <LiveSection :cardSet="card"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import LiveSection from './LiveSection';
+    export default {
+        name: 'Live',
+        components: {
+            LiveSection
+        },
+        data: function () {
+            return {
+               cards: [
+                   //    card start
+                   {
+                       image: 'grocery.png', 
+                       header: 'Litekart Grocery is a grocery flavour of litekart ecommerce stack', 
+                       paragraph: 'The project is written ground up based on cutting edge tech stack of 2020. NodeJS | GraphQL | Typescript | TailwindCSS | MongoDB | Vue makes it very powerfull and robust. Focussed on speed and customization capability.',
+                       link: '#',
+                       cardFlow: '',
+                        bottomCard:[
+                        {
+                            head: '121%',
+                            text: 'growth of daily visitors'
+                        },
+                        {
+                            head: '65%',
+                            text: 'growth of daily visitors'
+                        },
+                        {
+                            head: '100%',
+                            text: 'growth of daily visitors'
+                        }
+                    ]
+                    },
+                   //    card start
+                   {
+                       image: 'grocery.png', 
+                       header: 'Litekart Grocery is a grocery flavour of litekart ecommerce stack', 
+                       paragraph: 'The project is written ground up based on cutting edge tech stack of 2020. NodeJS | GraphQL | Typescript | TailwindCSS | MongoDB | Vue makes it very powerfull and robust. Focussed on speed and customization capability.',
+                       link: '#',
+                        cardFlow: 'reverse-container',
+                        bottomCard:[
+                       
+                        {
+                            head: '100%',
+                            text: 'growth of daily visitors'
+                        }
+                    ]
+                    },
+                   //    card start
+               ]
+            }
+        },
+    }
+</script>
+
+<style scoped>
+.project-heading {
+    margin-bottom: 4rem;
+    margin-top: 2rem;
+}
+
+.project-heading h2 {
+    text-align: center;
+    font-size: 3rem;
+    font-weight: 700;
+    letter-spacing: .05em;
+    color: #2d3754;
+    line-height: 1.8;
+}
+ .project-heading h2 span {
+    background-image: linear-gradient(90deg, #fa709a 0, #fee140);
+    background-repeat: no-repeat;
+    background-size: 100% .2em;
+    background-position: 0 100%;
+    transition: background-size .25s ease-in;
+    text-decoration: none;
+    padding-bottom: 3px;
+}
+</style>
